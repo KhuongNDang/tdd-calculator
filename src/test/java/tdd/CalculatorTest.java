be68f798c3562049a91233ec74500a6882e07b74
KhuongNDang <khndang@gmail.com>
2025-01-30 19:07:45 +0100

    Adding the project to the GitHub repository

---
+++ src/test/java/tdd/CalculatorTest.java
@@ -0,0 +1,21 @@
+package tdd;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    Calculator test = new Calculator();
+
+    @Test
+    public void setTest(){
+
+    assertEquals(8, test.add(3,5));
+    assertEquals(4, test.subtract(10,6));
+    assertEquals(6, test.multiply(2,3));
+    assertEquals(3, test.divide(9,3));
+
+
+    }
+}
